9670d124585bd363ce6953f0331cfb6c7d0050d7
mael <mael.minard@epita.fr>
2025-10-16 09:36:22 +0200

    nvim surround

--- home/modules/nixvim/plugins/nvim-surround.nix
+++ home/modules/nixvim/plugins/nvim-surround.nix
@@ -3,10 +3,9 @@
         enable = true;
         settings = {
             keymaps = {
-                insert = "<C-g>s";
-                insert_line = "<C-g>S";
                 normal = "ya";
                 normal_line = "yA";
+                delete = "da";
             };
         };
     };
